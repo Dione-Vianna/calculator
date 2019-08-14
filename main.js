534fdbdee4150e04f6069d704b12edcb11ff1aa5
Dione-Vianna <dineg12@hotmail.com>
2019-08-14 01:09:11 -0300

    criado a logica em javascript

---
+++ main.js
@@ -0,0 +1,53 @@
+// função que adiciona números ao visor
+function valor(numero){
+  if (typeof gvisor == 'undefined'){
+    document.calculadora.visor.value='';
+  }
+  document.calculadora.visor.value = document.calculadora.visor.value + numero;
+  gvisor = 1;
+}
+
+// função que limpa o visor
+function Limpar(){
+  document.calculadora.visor.value = '';
+  delete gvalor;
+  delete gnumero;
+  delete gvisor;
+}
+
+// função que executa as operações
+function calcOper(cal, valor1, valor2){
+  if(cal == "adicao"){
+    var valor = parseFloat(valor1) + parseFloat(valor2);
+  } else if (cal == "subtracao"){
+    var valor = valor1 - valor2;
+  } else if (cal == "multiplicacao"){
+    var valor = valor1 * valor2;
+  } else {
+    var valor = valor1/valor2;
+  }
+  return (valor);
+}
+
+// algoritimo de passagem das ações do usiuário
+function calculo(cal){
+  var valor = document.calculadora.visor.value;
+  delete gvisor;
+
+  if (typeof gnumero != 'undefined' && cal == 'resultado') {
+    gvalor = calcOper(gnumero, gvalor, valor);
+    document.calculadora.visor.value = gvalor;
+    delete cal;
+    delete gvalor;
+    return(0);
+  }
+
+  if (typeof gvalor != 'undefined'){
+    gvalor = calcOper(gnumero, gvalor, valor);
+    gnumero = cal;
+    document.calculadora.visor.value = gvalor;
+  } else {
+    gvalor = valor;
+    gnumero = cal;
+  }
+}
